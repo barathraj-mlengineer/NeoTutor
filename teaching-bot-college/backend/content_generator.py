2274f8ed5d88e00c328309d9271e94a536069d2f
Barath raj <sbarathraj.ai@gmail.com>
2025-08-08 14:47:00 +0530

    Update content_generator.py

--- teaching-bot-college/backend/content_generator.py
+++ teaching-bot-college/backend/content_generator.py
@@ -2,7 +2,7 @@ import requests
 from utils.config import load_api_key
 
 def generate_content(topic):
-    api_key ="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"  # Or hardcode for now if needed
+    api_key ="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"  # Or hardcode for now if needed
     url = "https://api.groq.com/openai/v1/chat/completions"
 
     headers = {
